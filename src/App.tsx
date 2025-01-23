1899f39436078c9df6546366e067738a67054666
Vinay Vishwakarma <vinayvishwakarma340@gmail.com>
2025-01-23 12:30:08 +0530

    dfs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Register from './pages/Register';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-    <div>Vinay This side</div>
+       <h1 className="text-red-500">
+    Hello world!
+  </h1>
+
+    {/* <Register /> */}
     </div>
   );
 };
